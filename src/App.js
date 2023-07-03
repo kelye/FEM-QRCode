5a20dc1c564edc3e99da1d398731cce7885bec97
kelye <kelvinye2@gmail.com>
2023-07-03 16:22:45 +1000

    Finished Challenge

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <div className='qr-container'><img src={require("./image-qr-code.png")} width="286px" height="286px"></img></div>
+      <h2>Improve your front-end skills by building projects</h2> 
+      <h4>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</h4>
     </div>
   );
 }
